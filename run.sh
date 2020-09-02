1b7f3daf9d62bc6160ffaa7acc439b0f29f6654b
nivethan-moolya <nivethan.radhakrishnan@moolya.com>
2020-09-02 06:20:20 +0530

    docker41

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ npm install -g appium@1.15.1
 appium -v
 appium &
 
-cd /Users/nradhakrishnan/Library/Android/sdk/emulator
+cd
+pwd
+cd /Library/Android/sdk/emulator
+pwd
 ls
 emulator -list-avds
 emulator -avd Nexus_6_API_26
